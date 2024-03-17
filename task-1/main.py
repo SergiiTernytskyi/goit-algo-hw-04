d7c31b8b5726d0d68ed5b632dce51da26f1bf094
Sergii Ternytskyi <serhij86@gmail.com>
2024-03-17 18:01:31 +0200

    Changes after review

--- task-1/main.py
+++ task-1/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from processing import total_salary
 
-file_path = Path('task-1/salaries.txt')
+def main():
+    file_path = Path('salaries.txt')
+    try:
+        total, average = total_salary(file_path)
+        print(f"Total salary value: {total}, average salary value: {average}")
+    except TypeError:
+        print(f"Oops! Someyhing went wrong. Try again...")
 
 
-try:
-    total, average = total_salary(file_path)
-    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-except TypeError:
-    print(f"Oops! Someyhing went wrong. Try again...")
+if __name__ == "__main__":
+    main()
--- task-2/main.py
+++ task-2/main.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from processing import get_cats_info
 
-file_path = Path('task-2/cats_list.txt')
+def main():
+    file_path = Path('cats_list.txt')
+    cats = get_cats_info(file_path)
+    print(cats)
 
-cats = get_cats_info(file_path)
-print(get_cats_info(file_path))
 
+if __name__ == "__main__":
+    main()
